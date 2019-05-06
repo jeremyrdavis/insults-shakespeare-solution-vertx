da52f06711a93362a10112c9a8dc7b29206a863f
jeremyrdavis <jeremyrdavis@jeremyrdavis.com>
2019-05-06 09:33:41 -0400

    working on openshift but not using properties

--- src/main/java/com/redhat/summit2019/HttpApplication.java
+++ src/main/java/com/redhat/summit2019/HttpApplication.java
@@ -1,48 +1,104 @@
 package com.redhat.summit2019;
 
-import io.vertx.core.AbstractVerticle;
+import com.redhat.summit2019.model.Insult;
+import io.reactivex.Single;
 import io.vertx.core.Future;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import io.vertx.reactivex.ext.web.handler.BodyHandler;
+import io.vertx.reactivex.ext.web.handler.StaticHandler;
+
+import java.util.concurrent.TimeUnit;
 
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 public class HttpApplication extends AbstractVerticle {
 
   static final String template = "Hello, %s!";
 
+  WebClient webClient;
+
   @Override
-  public void start(Future<Void> startFuture) {
+  public void start(Future<Void> future) {
+
+    webClient = WebClient.create(vertx);
+    
     // Create a router object.
     Router router = Router.router(vertx);
 
     router.get("/api/greeting").handler(this::greeting);
+    router.get("/api/insult").handler(BodyHandler.create());
+    router.get("/api/insult").handler(this::insultHandler);
     router.get("/*").handler(StaticHandler.create());
 
     // Create the HTTP server and pass the "accept" method to the request handler.
     vertx
-            .createHttpServer()
-            .requestHandler(router)
-            .listen(
-                    // Retrieve the port from the configuration, default to 8080.
-                    config().getInteger("http.port", 8080), ar -> {
-                      if (ar.succeeded()) {
-                        System.out.println("Server started on port " + ar.result().actualPort());
-                      }
-                      startFuture.handle(ar.mapEmpty());
-                    });
+        .createHttpServer()
+        .requestHandler(router)
+        .listen(
+            // Retrieve the port from the configuration, default to 8080.
+            config().getInteger("http.port", 8080), ar -> {
+              if (ar.succeeded()) {
+                System.out.println("Server started on port " + ar.result().actualPort());
+                System.out.println("Config startup message " + config().getString("startup.message", "none"));
+              }else{
+                System.out.println("Server failed " + ar.cause());
+                future.fail(ar.cause());
+              }
+                future.handle(ar.mapEmpty());
+            });
 
   }
+
+  private void insultHandler(RoutingContext rc) {
+
+    System.out.println("insultHandler called");
+    System.out.println("noun.port: " + config().getInteger("noun.port"));
+    System.out.println("noun.url: " + config().getString("noun.url"));
+    System.out.println("adjective.port: " + config().getInteger("adjective.port"));
+    System.out.println("adjective.url:" + config().getString("adjective.url"));
+
+    Single<JsonObject> noun = webClient
+            .get(8080, "insult-nouns","/api/noun")
+            .rxSend()
+            .doOnSuccess(r -> System.out.println("noun" + r.bodyAsString()))
+            .map(HttpResponse::bodyAsJsonObject);
+
+    Single<JsonObject> adj1 = webClient
+            .get(8080, "insult-adjectives", "/api/adjective")
+            .rxSend()
+            .doOnSuccess(r -> System.out.println("adj1" + r.bodyAsString()))
+            .map(HttpResponse::bodyAsJsonObject);
+
+    Single<JsonObject> adj2 = webClient
+            .get(8080, "insult-adjectives", "/api/adjective")
+            .rxSend()
+            .doOnSuccess(r -> System.out.println("adj2" + r.bodyAsString()))
+            .map(HttpResponse::bodyAsJsonObject);
+
+    Single.zip(
+            adj1.doOnError(error -> System.out.println(error.getMessage())),
+            adj2.doOnError(error -> System.out.println(error.getMessage())),
+            noun.doOnError(error -> System.out.println(error.getMessage())),
+            Insult::new)
+            .subscribe(r ->
+                    rc.response()
+                            .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
+                            .end(r.toString()));
+  }
+
+  private void error(RoutingContext rc, Throwable error){
+    rc.response()
+            .setStatusCode(500)
+            .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
+            .end(new JsonObject().put("error", error.getMessage()).encodePrettily());
+  }
+
+
 
   private void greeting(RoutingContext rc) {
     String name = rc.request().getParam("name");
@@ -57,5 +113,4 @@ public class HttpApplication extends AbstractVerticle {
         .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
         .end(response.encodePrettily());
   }
-
 }
